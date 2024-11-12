6b9e8769aaeb4353381ec6a873811d6bad861d0b
Reyes Cuesta Bootcamp QA <154596444+bootcampqa@users.noreply.github.com>
2024-11-12 19:47:07 +0100

    Update homepage_steps.js

--- cypress/e2e/step_definitions/homepage_steps.js
+++ cypress/e2e/step_definitions/homepage_steps.js
@@ -8,6 +8,8 @@ import {
   Given("the user visit homepage", () => {
     //Visita la pagina
     cy.visit("/quiz.php?id=10723");
+    cy.clearCookies();
+    cy.reload();
     //Acepta las cookies. Busca el boton que contenga el texto AGREE
     cy.contains("AGREE").click()
 
